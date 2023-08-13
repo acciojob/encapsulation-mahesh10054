41441034feae82fb38e5f74f1a076acba7bb95be
Mahesh Misal <maheshmisal2018@gmail.com>
2023-08-13 23:36:58 +0530

    Solutions

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[])
+  {
+      RWOnly rw = new RWOnly();
+      System.out.println(rw.getName());
+      rw.setName("Mahesh");
+      rw.getName();
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name = "Yogesh";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
